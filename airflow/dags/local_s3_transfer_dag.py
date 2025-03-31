486cd974e44b3a8007ef10c22873dc79e2c77954
hyeonjh <jhoon3697@naver.com>
2025-04-01 00:41:29 +0900

    dag 테스트용 파일일

--- airflow/dags/local_s3_transfer_dag.py
+++ airflow/dags/local_s3_transfer_dag.py
@@ -1,27 +1,27 @@
-# local_s3_transfer_dag.py
+# # local_s3_transfer_dag.py
 
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
-from task.upload_to_s3 import upload_m4a_to_s3
-from task.download_from_s3 import download_m4a_from_s3
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from datetime import datetime
+# from task.upload_to_s3 import upload_m4a_to_s3
+# from task.download_from_s3 import download_m4a_from_s3
 
-with DAG(
-    dag_id='local_s3_transfer_dag',
-    start_date=datetime(2025, 3, 23),
-    schedule_interval='@hourly',  # 수동 실행 or 테스트용
-    catchup=False
-) as dag:
+# with DAG(
+#     dag_id='local_s3_transfer_dag',
+#     start_date=datetime(2025, 3, 23),
+#     schedule_interval='@hourly',  # 수동 실행 or 테스트용
+#     catchup=False
+# ) as dag:
 
-    upload_task = PythonOperator(
-        task_id='upload_m4a_to_s3',
-        python_callable=upload_m4a_to_s3,
-        op_args=['/path/to/file', 'file_name']  # 예시 인자 추가 (파일 경로와 파일명)
-    )
+#     upload_task = PythonOperator(
+#         task_id='upload_m4a_to_s3',
+#         python_callable=upload_m4a_to_s3,
+#         op_args=['/path/to/file', 'file_name']  # 예시 인자 추가 (파일 경로와 파일명)
+#     )
 
-    download_task = PythonOperator(
-        task_id='download_m4a_from_s3',
-        python_callable=download_m4a_from_s3
-    )
+#     download_task = PythonOperator(
+#         task_id='download_m4a_from_s3',
+#         python_callable=download_m4a_from_s3
+#     )
 
-    upload_task >> download_task
+#     upload_task >> download_task
--- airflow/dags/task/upload_to_s3.py
+++ airflow/dags/task/upload_to_s3.py
@@ -1,13 +1,13 @@
-# upload_to_s3.py
+# # upload_to_s3.py
 
-import os
-from aws_conn import get_s3_client  # aws_conn.py에서 S3 클라이언트 가져오기
-from dotenv import load_dotenv
+# import os
+# from aws_conn import get_s3_client  # aws_conn.py에서 S3 클라이언트 가져오기
+# from dotenv import load_dotenv
 
-load_dotenv()  
+# load_dotenv()  
 
-def upload_m4a_to_s3(file_path, file_name):
-    s3_client = get_s3_client()  # S3 클라이언트를 한 번만 생성하여 가져오기
-    bucket_name = os.getenv('S3_BUCKET_NAME')  # 환경 변수에서 버킷 이름 가져오기
-    s3_client.upload_file(file_path, bucket_name, file_name)
-    print(f"✅ {file_name} S3에 업로드 완료")
+# def upload_m4a_to_s3(file_path, file_name):
+#     s3_client = get_s3_client()  # S3 클라이언트를 한 번만 생성하여 가져오기
+#     bucket_name = os.getenv('S3_BUCKET_NAME')  # 환경 변수에서 버킷 이름 가져오기
+#     s3_client.upload_file(file_path, bucket_name, file_name)
+#     print(f"✅ {file_name} S3에 업로드 완료")
